826391c5f4353ead84e70025f9849f461f1c69f0
Torin Balts <torinbalts@gmail.com>
2016-09-29 11:00:34 +0100

    Something something dark side

--- baseDados/codigo/clinica.sql
+++ baseDados/codigo/clinica.sql
@@ -1,49 +1,61 @@
--- nome do ficheiro: clinica.sql 
--- Telmo Silva 
--- Data Criacao: 26/09/2016 
+-- nome ficheiro = clinica.sql
+-- autor Telmo Silva
+-- data 29/9/2016
 
-CREATE DATABASE clinica; USE clinica;
 
---TABELA ESPECIALIDADE
-CREATE TABLE `especialidade`(
-    
-    `id_esp`        TINYINT(3) NOT NULL AUTO_INCREMENT,
-    `designacao`    VARCHAR(40) NOT NULL,
-    PRIMARY KEY(`id_esp`)
+-- TABELA ESPECIALIDADES
+CREATE TABLE `especialidades`
+(
+    `id_esp` TINYINT(3) NOT NULL AUTO_INCREMENT,
+    `designacao` VARCHAR(40) NOT NULL,
 
+    PRIMARY KEY (`id_esp`)
 ) ENGINE = INNODB DEFAULT CHARSET = latin1;
 
-
---TABELA PACIENTES
-CREATE TABLE `pacientes` (
-    
-    `id_paciente`   TINYINT(3) NOT NULL AUTO_INCREMENT,
-    `nome_paciente` VARCHAR(60) NOT NULL, 
-    `morada`        VARCHAR(100),
-    `contacto`      TINYINT(9),
-    `cod_postal`    VARCHAR(8),
-
-    PRIMARY KEY(`id_paciente`)
-
-) ENGINE = INNODB DEFAULT CHARSET = latin1;
-
-<<<<<<< HEAD
---TABELA MEDICOS
-CREATE TABLE `medicos`(
-
-=======
 -- TABELA MEDICOS
 CREATE TABLE `medicos`
 (
->>>>>>> 852b466ff97050dee6d67c06dcb101e75476d799
-    `id_medico`     TINYINT(3) NOT NULL AUTO_INCREMENT,
-    `nome_medico`   VARCHAR(60) NOT NULL,
-    `morada`        VARCHAR(100),
-    `contacto`      TINYINT(9),
-    `cod_postal`    VARCHAR(8),
-    `celula`        VARCHAR(5),
-    `id_esp`        TINYINT(3),
+    `id_medico` TINYINT(3) NOT NULL AUTO_INCREMENT,
+    `id_esp` TINYINT(3), 
+    `nome_medico` VARCHAR(40) NOT NULL,
 
     PRIMARY KEY(`id_medico`),
-    FOREIGN KEY(`id_esp`) REFERENCES `especialidades`(id_esp)
+    FOREIGN KEY(`id_esp`) REFERENCES especialidades(`id_esp`)
+) ENGINE = INNODB DEFAULT CHARSET = latin1;
+
+-- TABELA PACIENTES
+CREATE TABLE `pacientes`
+(
+    `id_paciente` TINYINT(3) NOT NULL AUTO_INCREMENT,
+    `nome_paciente` VARCHAR(40) NOT NULL,
+
+    PRIMARY KEY (`id_paciente`)
+
+) ENGINE = INNODB DEFAULT CHARSET = latin1;
+
+-- TABELA CONSULTAS
+CREATE TABLE `consultas`
+(
+    `id_consulta` TINYINT(3) NOT NULL AUTO_INCREMENT,
+    `id_medico` TINYINT(3),
+    `id_paciente` TINYINT(3),
+    `id_exame` TINYINT(3) NULL,
+    `data_consulta` DATE,
+    `hora_consulta` TIME,
+    `preco` DECIMAL(5,2),
+
+    PRIMARY KEY (`id_consulta`),
+    FOREIGN KEY (`id_medico`) REFERENCES medicos(`id_medico`),
+    FOREIGN KEY (`id_paciente`) REFERENCES pacientes(`id_paciente`),
+    FOREIGN KEY (`id_exame`) REFERENCES exames_comp(`id_exame`)
+
+) ENGINE = INNODB DEFAULT CHARSET = latin1;
+
+-- TABELA EXAMES
+CREATE TABLE `exames_comp`
+(
+    `id_exame` TINYINT(3) NOT NULL AUTO_INCREMENT,
+    `descricao` VARCHAR(40),
+
+    PRIMARY KEY (`id_exame`)
 ) ENGINE = INNODB DEFAULT CHARSET = latin1;
